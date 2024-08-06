3bc763894579aaae63afa1bf3bfcaacf6a5d3665
Leonardo Luis Tozoni <leocebe2000@gmail.com>
2024-08-06 11:10:15 -0300

    feat: create new component header and refactoring project clean archtecture

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from 'lucide-react';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { Card, CardContent } from './ui/card';
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 flex flex-row items-center justify-between">
+        <Image alt="Barber Logo" src="/logo.png" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
