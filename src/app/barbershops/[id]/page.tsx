a5fc10c05d37f31e348c64dc5ea762c36d84fdf0
Leonardo Luis Tozoni <leocebe2000@gmail.com>
2024-08-12 12:24:31 -0300

    feat: add sidebar sheet component

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import PhoneItem from '@/app/_components/PhoneItem';
 import ServiceItem from '@/app/_components/ServiceItem';
+import SidebarSheet from '@/app/_components/SidebarSheet';
 import { Button } from '@/app/_components/ui/button';
+import { Sheet, SheetTrigger } from '@/app/_components/ui/sheet';
 import { db } from '@/app/_lib/prisma';
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
 import Image from 'next/image';
@@ -48,13 +50,18 @@ const BarbershopPage = async ({ params }: IBarbershopPageProps) => {
           </Link>
         </Button>
 
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute right-4 top-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </div>
 
       <div className="p-5 border-b border-solid">
