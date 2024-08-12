45d746275d535910ec4314bb32624a998699604d
Leonardo Luis Tozoni <leocebe2000@gmail.com>
2024-08-12 11:33:36 -0300

    fix: correction style menu icon button

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -52,7 +52,6 @@ const BarbershopPage = async ({ params }: IBarbershopPageProps) => {
           size="icon"
           variant="secondary"
           className="absolute right-4 top-4"
-          asChild
         >
           <MenuIcon />
         </Button>
