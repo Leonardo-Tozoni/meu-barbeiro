ee1ab320da498fe24a9eed41bab172e43437f9ae
Leonardo Luis Tozoni <leocebe2000@gmail.com>
2024-08-12 21:48:55 -0300

    chore: add next auth setup

--- src/app/_components/SidebarSheet.tsx
+++ src/app/_components/SidebarSheet.tsx
@@ -1,9 +1,16 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from 'lucide-react';
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { quickSearchOptions } from '../_constants/search';
-import { Avatar, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from './ui/dialog';
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet';
 
 const SidebarSheet = () => {
@@ -13,15 +20,40 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="py-5 flex items-center border-b border-solid gap-3">
-        <Avatar>
+      <div className="py-5 flex justify-between items-center border-b border-solid gap-3">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-lg">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+              <Button variant="outline" className="flex gap-2 font-bold">
+                <Image
+                  alt="Logo do google"
+                  src="/google.svg"
+                  width={18}
+                  height={18}
+                />
+                Google
+              </Button>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://github.com/leonardo-tozoni.png" />
         </Avatar>
 
         <div>
           <p className="font-bold">Leonardo Tozoni</p>
           <p className="text-xs">leocebe2000@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="py-5 flex flex-col gap-2 border-b border-solid">
